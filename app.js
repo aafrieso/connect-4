6e7a716c68f023fb616194cccbb532ad154eb375
Austin Friesorger <aafrieso@svsu.edu>
2022-12-19 11:05:28 -0700

    start DOM content

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 document.addEventListener('DomContentLoaded', () => {
-    
+    const circles = document.querySelectorAll('gameboard')
+    const message = document.getElementById('message')
+    const showPlayerTurn 
 })
