bd42d5415dca103a781a702493b0e7e9168164aa
Austin Friesorger <aafrieso@svsu.edu>
2022-12-19 11:10:34 -0700

    add player turns

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 document.addEventListener('DomContentLoaded', () => {
     const circles = document.querySelectorAll('gameboard')
     const message = document.getElementById('message')
-    const showPlayerTurn 
+    const showPlayerTurn = document.getElementById('current-player')
+    let startPlayer = 1
 })
